ea0907f76fbc31976c60a2d025434422955fe855
krancce <jerryyangyilin@outlook.com>
2020-07-17 03:09:45 -0400

    Add files via upload

---
+++ reverseWords.java
@@ -0,0 +1,10 @@
+public class reverseWords{
+  public String reverseWords(String s) {
+    // remove leading spaces
+    s = s.trim();
+    // split by multiple spaces
+    List<String> wordList = Arrays.asList(s.split("\\s+"));
+    Collections.reverse(wordList);
+    return String.join(" ", wordList);
+  }
+}
